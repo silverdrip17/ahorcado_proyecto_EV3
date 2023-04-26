965bc637facc5a0ad25387a18113c4014e74ce91
Danu Litvac <danulitvac02@gmail.com>
2023-04-26 10:10:46 +0200

    botones cerrar

--- Ahorcado/FrmBienvenida.Designer.vb
+++ Ahorcado/FrmBienvenida.Designer.vb
@@ -33,10 +33,10 @@ Partial Class FrmBienvenida
         Me.btnJugar.BackColor = System.Drawing.Color.CornflowerBlue
         Me.btnJugar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnJugar.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnJugar.Location = New System.Drawing.Point(207, 94)
-        Me.btnJugar.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnJugar.Location = New System.Drawing.Point(155, 76)
+        Me.btnJugar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnJugar.Name = "btnJugar"
-        Me.btnJugar.Size = New System.Drawing.Size(145, 44)
+        Me.btnJugar.Size = New System.Drawing.Size(109, 36)
         Me.btnJugar.TabIndex = 1
         Me.btnJugar.Text = "Jugar"
         Me.btnJugar.UseVisualStyleBackColor = False
@@ -46,10 +46,10 @@ Partial Class FrmBienvenida
         Me.btnSalir.BackColor = System.Drawing.Color.Red
         Me.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnSalir.Location = New System.Drawing.Point(207, 288)
-        Me.btnSalir.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnSalir.Location = New System.Drawing.Point(155, 234)
+        Me.btnSalir.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnSalir.Name = "btnSalir"
-        Me.btnSalir.Size = New System.Drawing.Size(145, 44)
+        Me.btnSalir.Size = New System.Drawing.Size(109, 36)
         Me.btnSalir.TabIndex = 2
         Me.btnSalir.Text = "Salir"
         Me.btnSalir.UseVisualStyleBackColor = False
@@ -59,10 +59,10 @@ Partial Class FrmBienvenida
         Me.btnInstrucciones.BackColor = System.Drawing.Color.CornflowerBlue
         Me.btnInstrucciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnInstrucciones.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnInstrucciones.Location = New System.Drawing.Point(207, 164)
-        Me.btnInstrucciones.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnInstrucciones.Location = New System.Drawing.Point(155, 133)
+        Me.btnInstrucciones.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnInstrucciones.Name = "btnInstrucciones"
-        Me.btnInstrucciones.Size = New System.Drawing.Size(145, 44)
+        Me.btnInstrucciones.Size = New System.Drawing.Size(109, 36)
         Me.btnInstrucciones.TabIndex = 3
         Me.btnInstrucciones.Text = "Instrucciones"
         Me.btnInstrucciones.UseVisualStyleBackColor = False
@@ -72,27 +72,26 @@ Partial Class FrmBienvenida
         Me.BtnLoginAdmin.BackColor = System.Drawing.Color.CornflowerBlue
         Me.BtnLoginAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.BtnLoginAdmin.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnLoginAdmin.Location = New System.Drawing.Point(207, 225)
-        Me.BtnLoginAdmin.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BtnLoginAdmin.Location = New System.Drawing.Point(155, 183)
+        Me.BtnLoginAdmin.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnLoginAdmin.Name = "BtnLoginAdmin"
-        Me.BtnLoginAdmin.Size = New System.Drawing.Size(145, 44)
+        Me.BtnLoginAdmin.Size = New System.Drawing.Size(109, 36)
         Me.BtnLoginAdmin.TabIndex = 4
         Me.BtnLoginAdmin.Text = "Admin"
         Me.BtnLoginAdmin.UseVisualStyleBackColor = False
         '
         'FrmBienvenida
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.Ahorcado.My.Resources.Resources.ahorcado2
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(821, 494)
-        Me.ControlBox = False
+        Me.ClientSize = New System.Drawing.Size(616, 401)
         Me.Controls.Add(Me.BtnLoginAdmin)
         Me.Controls.Add(Me.btnInstrucciones)
         Me.Controls.Add(Me.btnSalir)
         Me.Controls.Add(Me.btnJugar)
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "FrmBienvenida"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Ahorcado"
