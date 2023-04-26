e5f1f299119fea68217d51ee00e828fe96f9efc6
Danu Litvac <danulitvac02@gmail.com>
2023-04-26 10:19:58 +0200

    retoque del control del usuario

--- Ahorcado/FrmBienvenida.vb
+++ Ahorcado/FrmBienvenida.vb
@@ -1,21 +1,19 @@
 ﻿Public Class FrmBienvenida
-    Dim contadorPulsados As Integer
 
     Private Sub btnJugar_Click(sender As Object, e As EventArgs) Handles btnJugar.Click
 
         'TODO preguntar si hay usuario o no
-        contadorPulsados += 1
 
+        Dim miControl As Control() = Me.Controls.Find("txtNombreUsuario", True)
 
-
-        If contadorPulsados = 1 Then
+        If miControl.Length = 0 Then
             Dim txtCaja As New TextBox
             txtCaja.Name = "txtNombreUsuario"
             txtCaja.Size = New Size(100, 10)
             txtCaja.Location = New Point(30, 50)
             Me.Controls.Add(txtCaja)
         Else
-            Dim miControl As Control() = Me.Controls.Find("txtNombreUsuario", True)
+
             If miControl(0).Text <> "" Then
                 ranking.Usuarios.Add(miControl(0).Text)
                 miControl(0).Text = ""
