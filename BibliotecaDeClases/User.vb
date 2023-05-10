cd1dda0b4adf0d33bbedebc5f13e36644cdfbd65
Danu Litvac <danulitvac02@gmail.com>
2023-05-10 10:20:59 +0200

    ranking arreglos de puntos

--- BibliotecaDeClases/User.vb
+++ BibliotecaDeClases/User.vb
@@ -2,12 +2,16 @@
     Public Property TiempoRespuesta As Integer
     Public Property Usuario As String
     Public Property Rondas As Integer
+    Private  puntucion As Integer
+
     Public Function GetFinalRanking() As String
         Return $"···Ranking···
                 Usuario : {Usuario} --> {Rondas} rondas en {TiempoRespuesta} segundos."
     End Function
     Public Sub GuardarUsuario()
-        Dim usuarioCompleto As String() = {$"{_Usuario}*{_Rondas}*{_TiempoRespuesta}"}
+        puntucion = Rondas * 100 - TiempoRespuesta
+
+        Dim usuarioCompleto As String() = {$"{_Usuario}*{puntucion}"}
 
 
         System.IO.File.AppendAllLines($"./Soluciones/Usarios.txt", usuarioCompleto)
