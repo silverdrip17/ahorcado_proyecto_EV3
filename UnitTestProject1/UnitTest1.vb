ea11c1cd98a14317491a8be8a11a041851d84af4
kevin lopes <kevinlopes566@gmail.com>
2023-05-22 08:33:31 +0200

    pruevas unitarias

--- UnitTestProject1/UnitTest1.vb
+++ UnitTestProject1/UnitTest1.vb
@@ -1,9 +1,20 @@
-﻿Imports System.Text
+﻿Imports System.IO
+Imports BibliotecaDeClases
+Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 <TestClass()> Public Class UnitTest1
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub testPalabraAAdivinar()
+        Dim path As String = "./Soluciones/soluciones.txt"
+        Dim palabras As String() = File.ReadAllLines("./Soluciones/soluciones.txt")
+        Dim listaPalabras As New ListaPalabras(True)
+        Dim actual As String = listaPalabras.PalabraAAdivinar()
+        For Each
+
+        Dim palabrasBien As String() = palabras.
+        Assert.IsTrue(palabras.Contains(actual))
+
     End Sub
 
 End Class
