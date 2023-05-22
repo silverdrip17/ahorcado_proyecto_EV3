de8289dd2562a288d7139949066db363074293ad
kevin lopes <kevinlopes566@gmail.com>
2023-05-22 09:05:23 +0200

    pruebas unitarias terminadas

--- UnitTestProject1/UnitTest1.vb
+++ UnitTestProject1/UnitTest1.vb
@@ -2,19 +2,48 @@
 Imports BibliotecaDeClases
 Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports System.Diagnostics.Contracts
+Imports System.Security.Cryptography.X509Certificates
 
 <TestClass()> Public Class UnitTest1
 
     <TestMethod()> Public Sub testPalabraAAdivinar()
-        Dim path As String = "./Soluciones/soluciones.txt"
         Dim palabras As String() = File.ReadAllLines("./Soluciones/soluciones.txt")
         Dim listaPalabras As New ListaPalabras(True)
         Dim actual As String = listaPalabras.PalabraAAdivinar()
-        For Each
+        Dim palabrasBien As New List(Of String)
+        For Each palabra In palabras
+            Dim arrayalabras As String() = palabra.Split("*")
 
-        Dim palabrasBien As String() = palabras.
-        Assert.IsTrue(palabras.Contains(actual))
+            palabrasBien.Add(arrayalabras(0))
+        Next
+
+        For Each palabra In palabrasBien
+            If palabra.Equals(actual) Then
+                Assert.IsTrue(True)
+            End If
+        Next
 
     End Sub
 
+    '<TestClass()> Public Class UnitTest2
+    <TestMethod()> Public Sub testPalabraAAdivinarFacil()
+            Dim palabras As String() = File.ReadAllLines("./Soluciones/TextFile1.txt")
+            Dim listaPalabras As New ListaPalabras(True)
+            Dim actual As String = listaPalabras.PalabraAAdivinar()
+            Dim palabrasBien As New List(Of String)
+            For Each palabra In palabras
+                Dim arrayalabras As String() = palabra.Split("*")
+
+                palabrasBien.Add(arrayalabras(0))
+            Next
+
+            For Each palabra In palabrasBien
+                If palabra.Equals(actual) Then
+                    Assert.IsTrue(True)
+                End If
+            Next
+        End Sub
+
+    'End Class
 End Class
